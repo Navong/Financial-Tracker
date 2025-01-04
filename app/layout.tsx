2414c4f14ab607e2884b6d3fb05a8387576177b6
nolan <nolan@mac.com>
2025-01-05 03:07:06 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import { Navbar } from './components/Navbar'
 import Footer from './components/Footer'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
 import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -24,14 +24,19 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Providers>
-            <div className="flex flex-col min-h-screen">
-              <Navbar />
-              <main className="flex-1 p-8 max-w-7xl mx-auto w-full">
-                {children}
-                <Toaster />
-              </main>
-              <Footer />
-            </div>
+            <SignedIn>
+              <div className="flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-1 p-8 max-w-7xl mx-auto w-full">
+                  {children}
+                  <Toaster />
+                </main>
+                <Footer />
+              </div>
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
           </Providers>
         </body>
       </html>
